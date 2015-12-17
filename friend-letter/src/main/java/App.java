5268e7dfcff5b80c91dcfd7c1e1ed8f7abba4fcc
Jeffrey Ruder <ruderjt@gmail.com>
2015-12-16 20:13:01 -0800

    Add travel links page to practice site

--- friend-letter/src/main/java/App.java
+++ friend-letter/src/main/java/App.java
@@ -3,7 +3,7 @@ import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
-    
+
     get("/", (request, response) ->
         "<!DOCTYPE html>" +
         "<html>" +
@@ -20,27 +20,44 @@ public class App {
            "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
            "<p>Cheers,</p>" +
            "<p>Travel Enthusiast Jeff</p>" +
-           "<p><a href='/favorite_photos' >P.S. Check out my favorite travel photos here.</a></p>" +
+           "<p><a href='/favorite_photos'>P.S. Check out my favorite travel photos here.</a></p>" +
+           "<p><a href='/travel_links'>P.P.S. Check out some useful travel links here.</a></p>" +
          "</body>" +
        "</html>"
     );
 
     get("/favorite_photos", (request, response) ->
         "<!DOCTYPE html>" +
-          "<html>" +
-            "<head>" +
-              "<title>Hello Friend!</title>" +
-              "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>" +
-            "</head>" +
-            "<body>" +
-              "<h1>Favorite Traveling Photos</h1>" +
-                "<ul>" +
-                  "<li><img src='/images/canyon.jpg' alt='A photo of a canyon.'/></li>" +
-                  "<li><img src='/images/city.jpg' alt='A photo of a bridge.'/></li>" +
-                  "<li><img src='/images/mountains.jpg' alt='A photo of a mountain.'/></li>" +
-                "</ul>" +
-            "</body>" +
-          "</html>"
+        "<html>" +
+          "<head>" +
+            "<title>Hello Friend!</title>" +
+            "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>" +
+          "</head>" +
+          "<body>" +
+            "<h1>Favorite Traveling Photos</h1>" +
+            "<ul>" +
+              "<li><img src='/images/canyon.jpg' alt='A photo of a canyon.'/></li>" +
+              "<li><img src='/images/city.jpg' alt='A photo of a bridge.'/></li>" +
+              "<li><img src='/images/mountains.jpg' alt='A photo of a mountain.'/></li>" +
+            "</ul>" +
+          "</body>" +
+        "</html>"
+    );
+
+    get("/travel_links", (request, response) ->
+      "<!DOCTYPE html>" +
+      "<html>" +
+        "<head>" +
+          "<title>Useful Links</title>" +
+          "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>" +
+        "</head>" +
+        "<body>" +
+          "<h1>Useful Travel Links</h1>" +
+          "<ul>" +
+            "<li><a href='http://www.kayak.com'>Kayak</a></li>" +
+          "</ul>" +
+        "</body>" +
+      "</html>"
     );
   }
 }
